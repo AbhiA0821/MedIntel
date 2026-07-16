e35fee77fe8224b9dc075da62bcbb6eb8eb9600c
Abhi0821 <ainapureabhi0821@gmail.com>
2026-07-16 19:10:53 +0530

    Implement critical patient reason detection

--- database/monitoring_queries.py
+++ database/monitoring_queries.py
@@ -1,6 +1,8 @@
 import duckdb
 con=duckdb.connect("database/medintel.duckdb")
 
+
+#Cheaking which are criical
 def get_critical_patients():
     query="""
     SELECT
@@ -22,9 +24,39 @@ WHERE
 
     return con.execute(query).fetchall()
 
+
+#Reason for patient critical conditon
+def get_patient_reasons(patient):
+    reasons=[]
+    heart_rate = patient[2]
+    spo2 = patient[3]
+    temperature = patient[4]
+    systolic_bp = patient[5]
+    if spo2 < 92:
+        reasons.append("Low Oxygen")
+    if heart_rate > 100:
+        reasons.append("High Heart Rate")
+    if temperature > 38:
+        reasons.append("High Temperature")    
+    if systolic_bp > 140:
+        reasons.append("High Blood Pressure")
+
+    return reasons
+
+
 critical_patients = get_critical_patients()
 
+#calling function for eah patientthat return name and reason
 for patient in critical_patients:
-    print(patient)
+    reasons = get_patient_reasons(patient)
+    print("=" * 50)
+    print(f"Patient : {patient[0]} {patient[1]}")
+    
+    print("Reasons:")
+    for reason in reasons:
+        print(f"{reason}")
+
+
+
 
 con.close()
