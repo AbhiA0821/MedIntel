304e3454426871230082b257d5b9d43423b24a4e
Abhi0821 <ainapureabhi0821@gmail.com>
2026-07-21 23:39:53 +0530

    Implement patient vitals simulator with realistic health data generation

---
+++ simulator/patient_simulator.py
@@ -0,0 +1,117 @@
+import random
+
+
+# ----------------------------------------
+# Heart Rate
+# ----------------------------------------
+def generate_heart_rate():
+
+    chance = random.randint(1, 100)
+
+    if chance <= 80:
+        return random.randint(60, 100)      # Normal
+
+    elif chance <= 95:
+        return random.randint(101, 140)     # High
+
+    else:
+        return random.randint(40, 59)       # Low
+
+
+# ----------------------------------------
+# SpO2
+# ----------------------------------------
+def generate_spo2():
+
+    chance = random.randint(1, 100)
+
+    if chance <= 85:
+        return random.randint(95, 100)      # Normal
+
+    elif chance <= 95:
+        return random.randint(88, 91)       # Low
+
+    else:
+        return random.randint(80, 87)       # Critical
+
+
+# ----------------------------------------
+# Temperature
+# ----------------------------------------
+def generate_temperature():
+
+    chance = random.randint(1, 100)
+
+    if chance <= 85:
+        return round(random.uniform(36.5, 37.5), 1)
+
+    else:
+        return round(random.uniform(38.0, 39.5), 1)
+
+
+# ----------------------------------------
+# Blood Pressure
+# ----------------------------------------
+def generate_blood_pressure():
+
+    chance = random.randint(1, 100)
+
+    if chance <= 80:
+
+        systolic = random.randint(100, 120)
+        diastolic = random.randint(65, 80)
+
+    else:
+
+        systolic = random.randint(141, 170)
+        diastolic = random.randint(90, 110)
+
+    return systolic, diastolic
+
+
+# ----------------------------------------
+# Respiratory Rate
+# ----------------------------------------
+def generate_respiratory_rate():
+
+    chance = random.randint(1, 100)
+
+    if chance <= 85:
+        return random.randint(12, 20)
+
+    elif chance <= 95:
+        return random.randint(21, 30)
+
+    else:
+        return random.randint(8, 11)
+
+
+# ----------------------------------------
+# Generate Complete Vitals
+# ----------------------------------------
+def generate_vitals():
+
+    systolic_bp, diastolic_bp = generate_blood_pressure()
+
+    return {
+        "heart_rate": generate_heart_rate(),
+        "spo2": generate_spo2(),
+        "temperature": generate_temperature(),
+        "systolic_bp": systolic_bp,
+        "diastolic_bp": diastolic_bp,
+        "respiratory_rate": generate_respiratory_rate()
+    }
+
+
+# ----------------------------------------
+# Test
+# ----------------------------------------
+if __name__ == "__main__":
+
+    print("Sample Generated Vitals\n")
+
+    for i in range(10):
+
+        print(f"Patient {i+1}")
+        print(generate_vitals())
+        print("-" * 60)
