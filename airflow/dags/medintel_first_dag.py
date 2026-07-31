1ba84d5f0e9fd92fe158a2389fa8afe8bba0ab88
Abhi0821 <ainapureabhi0821@gmail.com>
2026-07-31 15:33:06 +0530

    feat: update MedIntel Airflow DAG

--- airflow/dags/medintel_first_dag.py
+++ airflow/dags/medintel_first_dag.py
@@ -2,6 +2,8 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 
+
+
 with DAG(
     dag_id="medintel_first_dag",
     start_date=datetime(2026, 7, 31),
@@ -13,6 +15,8 @@ with DAG(
     def load_patient_data():
         print("Loading patient data...")
 
+
+#cretae a PythonOperator    Creates a task in DAG
     load_task = PythonOperator(
         task_id="load_patient_data",
         python_callable=load_patient_data,
